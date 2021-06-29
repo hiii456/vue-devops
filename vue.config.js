317a1e047cb7611eaa6d0e75618fe948879c97ad
최혜원 <choehyewon@choehyewon-ui-MacBookAir.local>
2021-06-29 14:31:03 +0900

    new commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-      publicPath: "/vue-devops/",
+      publicPath: "Users/choehyewon/Documents/취업준비/자기주도PJT/210629/vue-devops/vue-devops/",
       outputDir: "dist"
 };
